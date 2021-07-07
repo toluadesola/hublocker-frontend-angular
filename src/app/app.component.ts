566e21ce04180427ea9d0b67769cc68d38c76c11
toluadesola <t.toluadesola@gmail.com>
2021-07-07 23:48:33 +0100

    added search functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Locker } from './locker';
-import { Location } from './location';
 import { LockerService } from './locker.service';
 
 @Component({
@@ -9,27 +9,5 @@ import { LockerService } from './locker.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public lockers: Locker[] = [];
-
-  constructor(private lockerService: LockerService){}
-  ngOnInit(): void {
-    this.getLockers();
-  }
-
-  public getLockers(): void {
-    this.lockerService.getLockers().subscribe(
-      (response: Locker[]) => {
-        // console.log("In get")
-        this.lockers = response;
-        
-        for(let locker of this.lockers){
-          console.log(locker.location.city)
-        }
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    )
-  }
+export class AppComponent {
 }
